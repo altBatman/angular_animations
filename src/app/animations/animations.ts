d107046daa435366b21383eca824613c177e0ab0
Rajesh <r.mangena@mar-hro.de>
2021-10-18 15:47:04 +0200

    animation function Parameters
    using params Object in animation function

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -9,23 +9,31 @@ import {
   useAnimation,
 } from "@angular/animations";
 
-export const backOutLeftAnimation = animation([
-  style({ backgroundColor: "#FF759D" }),
-  animate(
-    "300ms",
-    keyframes([
-      style({
-        offset: 0,
-        opacity: 1,
-      }),
-      style({
-        offset: 1,
-        transform: " translate3d(-50%, 0, 0)",
-        opacity: 0.7,
-      }),
-    ])
-  ),
-]);
+export const backOutLeftAnimation = animation(
+  [
+    style({ backgroundColor: "#FF759D" }),
+    animate(
+      "{{duration}} {{easing}}",
+      keyframes([
+        style({
+          offset: 0,
+          opacity: 1,
+        }),
+        style({
+          offset: 1,
+          transform: "translate3d(-100%, 0, 0)",
+          opacity: 0.7,
+        }),
+      ])
+    ),
+  ],
+  {
+    params: {
+      duration: "500ms",
+      easing: "ease-out",
+    },
+  }
+);
 
 const fadeInAnimation = animation([style({ opacity: 0 }), animate(1000)]);
 const fadeOutAnimation = animation(animate(1000, style({ opacity: 0 })));
@@ -42,5 +50,8 @@ export const slide = trigger("slide", [
     animate(1000, style({ backgroundColor: "#bde78c", opacity: 1 })),
   ]),
 
-  transition(":leave", useAnimation(backOutLeftAnimation)),
+  transition(
+    ":leave",
+    useAnimation(backOutLeftAnimation, { params: { duration: "1000ms" } })
+  ),
 ]);
