4400ce7f0a4e7882f3c343a23003ccced8e6260f
Rajesh <r.mangena@mar-hro.de>
2021-10-18 15:36:27 +0200

    edited Html and Style animation Func

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -9,31 +9,23 @@ import {
   useAnimation,
 } from "@angular/animations";
 
-export const backOutLeftAnimation = animation(
+export const backOutLeftAnimation = animation([
+  style({ backgroundColor: "#FF759D" }),
   animate(
-    "500ms ease-in",
+    "300ms",
     keyframes([
       style({
         offset: 0,
-        transform: "scale(1)",
         opacity: 1,
-        backgroundColor: "#FF759D",
-      }),
-      style({
-        offset: 0.2,
-        transform: " translateX(0px) scale(0.7)",
-        opacity: 0.7,
-        backgroundColor: "#FF759D",
       }),
       style({
         offset: 1,
-        transform: " translateX(-50%) scale(0.7)",
+        transform: " translate3d(-50%, 0, 0)",
         opacity: 0.7,
-        backgroundColor: "#FF759D",
       }),
     ])
-  )
-);
+  ),
+]);
 
 const fadeInAnimation = animation([style({ opacity: 0 }), animate(1000)]);
 const fadeOutAnimation = animation(animate(1000, style({ opacity: 0 })));
